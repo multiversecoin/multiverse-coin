6d195a0d6e1af47a50e6418ac0baea50a25436a1
multiversecoin <mnsmns2009@hotmail.com>
2025-04-22 14:49:29 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,37 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MoedaMultiversoApp());
+}
+
+class MoedaMultiversoApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Moeda Multiverso',
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Moeda Multiverso'),
+      ),
+      body: Center(
+        child: Text(
+          'Bem-vindo à moeda digital de Moema!',
+          style: TextStyle(fontSize: 20),
+          textAlign: TextAlign.center,
+        ),
+      ),
+    );
+  }
+}
 
